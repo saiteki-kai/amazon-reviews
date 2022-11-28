27ccb8e066594f2046da5290b14fb60a7248addd
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-11-28 16:17:39 +0100

    feat: add dynamic fill for kind comparison

--- dashboard/pages/comparison.py
+++ dashboard/pages/comparison.py
@@ -1,5 +1,7 @@
 import dash_bootstrap_components as dbc
 from dash import Input, Output, callback, dcc, html
+from reviews.config import processed_data_dir
+import pandas as pd
 
 layout = html.Div(
     [
@@ -40,12 +42,22 @@ layout = html.Div(
     ]
 )
 
+# function for dynamic dash page -----------------------------------------------
+#variable
+data_df = pd.read_json(processed_data_dir / "products_reviews.json.gz")
 
-
-# function for dynamic dash page
+# dynamic set placeholder for dropdown left and right
 @callback([Output('dropdown_left', "placeholder"), Output('dropdown_right', "placeholder")], [Input('dropdown_down_comparison', 'value')])
-def drop_down_kind_comparison(value):
+def drop_down_placeholder_kind_comparison(value):
     if(str(value) == "None"):
         return "Select your kind of comparison", "Select your kind of comparison"
     else:
-        return "Select your " + str(value), "Select your " + str(value)
+        return "Select your " + str(value), "Select your " + str(value),
+
+# dynamic set value for dropdown left and right 
+@callback([Output('dropdown_left', "options"), Output('dropdown_right', "options")],[Input('dropdown_down_comparison', 'value')])
+def drop_down_value(value):
+    if(str(value) == "None"):
+        return [],[]
+    else:
+        return data_df["brand"].to_dict() , ["valori"]
