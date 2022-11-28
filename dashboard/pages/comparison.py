da123b55b1411ef18b9a750750b1def6200182ab
Mirco Malanchini <mircomalanchini@hotmail.it>
2022-11-28 14:21:30 +0100

    feat: add struct of comparison dash page

--- dashboard/pages/comparison.py
+++ dashboard/pages/comparison.py
@@ -2,10 +2,22 @@ import dash_bootstrap_components as dbc
 from dash import html
 
 layout = html.Div(
-    dbc.Container(
-        [
-            html.H1("Comparison"),
-        ],
-        className="py-3",
-    ),
+    [
+        dbc.Container(
+            [
+                html.H1("Comparison"),
+            ],
+            className="py-3",
+        ),        
+        dbc.Row([
+            dbc.Col(html.Div("prima linea grafici,primo grafico")),
+            dbc.Col(html.Div("prima linea grafici,secondo grafico")),
+            dbc.Col(html.Div("prima linea grafici,terzo grafico")),
+        ]),
+        dbc.Row([
+            dbc.Col(html.Div("seconda linea grafici, primo grafico")),
+            dbc.Col(html.Div("seconda linea grafici, secondo grafico")),
+        ]),
+    ]
 )
+
