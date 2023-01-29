ca8466521916ba2b42872f134e690b9da56457b5
saiteki-kai <giuseppe.magazu@gmail.com>
2023-01-29 22:09:54 +0100

    feat: update brand categories

--- dashboard/pages/sentiment/panels/category.py
+++ dashboard/pages/sentiment/panels/category.py
@@ -1,15 +1,12 @@
 import dash
-import dash_bootstrap_components as dbc
 from dash import Input, Output, dcc, html
 
 from dashboard.app import data_df
-from dashboard.components.category_barplot import category_barplot
 from dashboard.components.category_sentiment_barplot import category_sentiment_barplot
 from dashboard.utils import update_brand
 
 
 @dash.callback(
-    Output("categories-distribution", "figure"),
     Output("categories-distribution-perc", "figure"),
     Input("brand-select", "value"),
     Input("category-select", "value"),
@@ -18,24 +15,11 @@ def update_brand_categories(brand, category):
     # update graph brand
     brand_df = update_brand(data_df, brand, category)
     order = dict(category=list(brand_df["category"].value_counts().index))
-    return category_barplot(brand_df, order), category_sentiment_barplot(
-        brand_df, order
-    )
+
+    return category_sentiment_barplot(brand_df, order)
 
 
 panel = html.Div(
-    dbc.Row(
-        [
-            dbc.Col(
-                dcc.Graph(id="categories-distribution"),
-                className="h-100",
-            ),
-            dbc.Col(
-                dcc.Graph(id="categories-distribution-perc"),
-                className="h-100",
-            ),
-        ],
-        className="h-100",
-    ),
+    dcc.Graph(id="categories-distribution-perc", config={"displayModeBar": False}),
     className="panel",
 )
